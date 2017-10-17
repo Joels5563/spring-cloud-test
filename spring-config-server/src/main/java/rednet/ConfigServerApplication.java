fe674560adcf418b4221c98c6832a31e83f70ca7
Jeols Zhou <196001330@qq.com>
2017-10-17 11:29:01 +0800

    配置服务

---
+++ spring-config-server/src/main/java/rednet/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package rednet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author zhouchao
+ * @Description 配置文件服务器
+ * @date 2017/10/17 11:16
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
